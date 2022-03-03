164a9cbb1452cc5a797fe8781043cc198a25bb9b
HeCu22 <helgacuijpers@xs4all.nl>
2022-03-03 09:29:00 +0100

    countriestabel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 console.log('Hallo daar!');
+//
+// De API die ik ga gegbruiken staat bij RESTcountries.com version 2
+// Ik zie de volgende endpoint die ik ga gebruiken: Endpoint voor de api ALL: https://restcountries.com/v2/all
+// Als je de data bijkijkt zie je dat het een array is met objecten, waar ook weer arrays en objecten in staan
+//
+// Ik ga hiervanaf mappen en ineen nieuwe array zetten ik wil alleen alleen naam, population en flags.
+// Dat zijn dus de keynames.
+// Vervolgens ga ik mijn html voor elk een list item of heading teruggeven of attribuut.
+//
+// In javascript door de array heen loopen en eventueel een filter op dollar of find op region.
+// Vlag is een svg image.
+// Land is een header en population daaronder.
+// Een aantal data kopieren en een eigen array maken en vervolgens Hiermee testen hoe je je functie opbouwt.
+// 1. in html definieer ik eerst de header en centreer die in styles.
+// 2. De world-map eronder met een alt definitie.
+// 3. Maak een aparte main file waar de test-array in komt
+// 4. Zet waarden in de test array en loop er doorheen
+// 5. Zet een ul en li structuur in de html en ken hier id selectors aan toe;
+// 6. definieer een constante voor een nieuwe array country-info en map de naam population en flag;
+// definieer variabelen voor nodes in javascript en assign id-attributen
+// const get = document.getElementById("id-attribuut");
+// zet in de text content html document de in javascript berekende waardes
+// get.textContent = .....;
+
+
