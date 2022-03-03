9a6c2ada0219be52f277a52ae7ababc1c983caa4
HeCu22 <helgacuijpers@xs4all.nl>
2022-03-03 11:57:19 +0100

    countriesaxios

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,463 @@
+const countries = [
+    {
+        name: "Afghanistan",
+        "topLevelDomain": [],
+        "alpha2Code": "AF",
+        "alpha3Code": "AFG",
+        "callingCodes": [],
+        "capital": "Kabul",
+        "altSpellings": [],
+        "subregion": "Southern Asia",
+        "region": "Asia",
+        population: 40218234,
+        "latlng": [],
+        "demonym": "Afghan",
+        "area": 652230,
+        "timezones": [],
+        "borders": [],
+        "nativeName": "افغانستان",
+        "numericCode": "004",
+        "flags": {
+        },
+        "currencies": [],
+        "languages": [],
+        "translations": {
+        },
+        flag: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
+        "regionalBlocs": [],
+        "cioc": "AFG",
+        "independent": true
+    },
+    {
+        name: "Albania",
+        "topLevelDomain": [
+            ".al"
+        ],
+        "alpha2Code": "AL",
+        "alpha3Code": "ALB",
+        "callingCodes": [
+            "355"
+        ],
+        "capital": "Tirana",
+        "altSpellings": [
+            "AL",
+            "Shqipëri",
+            "Shqipëria",
+            "Shqipnia"
+        ],
+        "subregion": "Southern Europe",
+        "region": "Europe",
+        "population": 2837743,
+        "latlng": [
+            41,
+            20
+        ],
+        "demonym": "Albanian",
+        "area": 28748,
+        "gini": 33.2,
+        "timezones": [
+            "UTC+01:00"
+        ],
+        "borders": [
+            "MNE",
+            "GRC",
+            "MKD",
+            "UNK"
+        ],
+        "nativeName": "Shqipëria",
+        "numericCode": "008",
+        "flags": {
+            "svg": "https://flagcdn.com/al.svg",
+            "png": "https://flagcdn.com/w320/al.png"
+        },
+        "currencies": [
+            {
+                "code": "ALL",
+                "name": "Albanian lek",
+                "symbol": "L"
+            }
+        ],
+        "languages": [
+            {
+                "iso639_1": "sq",
+                "iso639_2": "sqi",
+                "name": "Albanian",
+                "nativeName": "Shqip"
+            }
+        ],
+        "translations": {
+            "br": "Albânia",
+            "pt": "Albânia",
+            "nl": "Albanië",
+            "hr": "Albanija",
+            "fa": "آلبانی",
+            "de": "Albanien",
+            "es": "Albania",
+            "fr": "Albanie",
+            "ja": "アルバニア",
+            "it": "Albania",
+            "hu": "Albánia"
+        },
+        "flag": "https://flagcdn.com/al.svg",
+        "regionalBlocs": [
+            {
+                "acronym": "CEFTA",
+                "name": "Central European Free Trade Agreement"
+            }
+        ],
+        "cioc": "ALB",
+        "independent": true
+    },
+    {
+        "name": "Algeria",
+        "topLevelDomain": [
+            ".dz"
+        ],
+        "alpha2Code": "DZ",
+        "alpha3Code": "DZA",
+        "callingCodes": [
+            "213"
+        ],
+        "capital": "Algiers",
+        "altSpellings": [
+            "DZ",
+            "Dzayer",
+            "Algérie"
+        ],
+        "subregion": "Northern Africa",
+        "region": "Africa",
+        "population": 43851043,
+        "latlng": [
+            28,
+            3
+        ],
+        "demonym": "Algerian",
+        "area": 2381741,
+        "gini": 27.6,
+        "timezones": [
+            "UTC+01:00"
+        ],
+        "borders": [
+            "TUN",
+            "LBY",
+            "NER",
+            "ESH",
+            "MRT",
+            "MLI",
+            "MAR"
+        ],
+        "nativeName": "الجزائر",
+        "numericCode": "012",
+        "flags": {
+            "svg": "https://flagcdn.com/dz.svg",
+            "png": "https://flagcdn.com/w320/dz.png"
+        },
+        "currencies": [
+            {
+                "code": "DZD",
+                "name": "Algerian dinar",
+                "symbol": "د.ج"
+            }
+        ],
+        "languages": [
+            {
+                "iso639_1": "ar",
+                "iso639_2": "ara",
+                "name": "Arabic",
+                "nativeName": "العربية"
+            }
+        ],
+        "translations": {
+            "br": "Argélia",
+            "pt": "Argélia",
+            "nl": "Algerije",
+            "hr": "Alžir",
+            "fa": "الجزایر",
+            "de": "Algerien",
+            "es": "Argelia",
+            "fr": "Algérie",
+            "ja": "アルジェリア",
+            "it": "Algeria",
+            "hu": "Algéria"
+        },
+        "flag": "https://flagcdn.com/dz.svg",
+        "regionalBlocs": [
+            {
+                "acronym": "AU",
+                "name": "African Union",
+                "otherNames": [
+                    "الاتحاد الأفريقي",
+                    "Union africaine",
+                    "União Africana",
+                    "Unión Africana",
+                    "Umoja wa Afrika"
+                ]
+            },
+            {
+                "acronym": "AL",
+                "name": "Arab League",
+                "otherNames": [
+                    "جامعة الدول العربية",
+                    "Jāmiʻat ad-Duwal al-ʻArabīyah",
+                    "League of Arab States"
+                ]
+            }
+        ],
+        "cioc": "ALG",
+        "independent": true
+    },
+    {
+        "name": "American Samoa",
+        "topLevelDomain": [
+            ".as"
+        ],
+        "alpha2Code": "AS",
+        "alpha3Code": "ASM",
+        "callingCodes": [
+            "1"
+        ],
+        "capital": "Pago Pago",
+        "altSpellings": [
+            "AS",
+            "Amerika Sāmoa",
+            "Amelika Sāmoa",
+            "Sāmoa Amelika"
+        ],
+        "subregion": "Polynesia",
+        "region": "Oceania",
+        "population": 55197,
+        "latlng": [
+            -14.33333333,
+            -170
+        ],
+        "demonym": "American Samoan",
+        "area": 199,
+        "timezones": [
+            "UTC-11:00"
+        ],
+        "nativeName": "American Samoa",
+        "numericCode": "016",
+        "flags": {
+            "svg": "https://flagcdn.com/as.svg",
+            "png": "https://flagcdn.com/w320/as.png"
+        },
+        "currencies": [
+            {
+                "code": "USD",
+                "name": "United States Dollar",
+                "symbol": "$"
+            }
+        ],
+        "languages": [
+            {
+                "iso639_1": "en",
+                "iso639_2": "eng",
+                "name": "English",
+                "nativeName": "English"
+            },
+            {
+                "iso639_1": "sm",
+                "iso639_2": "smo",
+                "name": "Samoan",
+                "nativeName": "gagana fa'a Samoa"
+            }
+        ],
+        "translations": {
+            "br": "Samoa Americana",
+            "pt": "Samoa Americana",
+            "nl": "Amerikaans Samoa",
+            "hr": "Američka Samoa",
+            "fa": "ساموآی آمریکا",
+            "de": "Amerikanisch-Samoa",
+            "es": "Samoa Americana",
+            "fr": "Samoa américaines",
+            "ja": "アメリカ領サモア",
+            "it": "Samoa Americane",
+            "hu": "Amerikai Szamoa"
+        },
+        "flag": "https://flagcdn.com/as.svg",
+        "cioc": "ASA",
+        "independent": false
+    },
+    {
+        "name": "Andorra",
+        "topLevelDomain": [
+            ".ad"
+        ],
+        "alpha2Code": "AD",
+        "alpha3Code": "AND",
+        "callingCodes": [
+            "376"
+        ],
+        "capital": "Andorra la Vella",
+        "altSpellings": [
+            "AD",
+            "Principality of Andorra",
+            "Principat d'Andorra"
+        ],
+        "subregion": "Southern Europe",
+        "region": "Europe",
+        "population": 77265,
+        "latlng": [
+            42.5,
+            1.5
+        ],
+        "demonym": "Andorran",
+        "area": 468,
+        "timezones": [
+            "UTC+01:00"
+        ],
+        "borders": [
+            "FRA",
+            "ESP"
+        ],
+        "nativeName": "Andorra",
+        "numericCode": "020",
+        "flags": {
+            "svg": "https://flagcdn.com/ad.svg",
+            "png": "https://flagcdn.com/w320/ad.png"
+        },
+        "currencies": [
+            {
+                "code": "EUR",
+                "name": "Euro",
+                "symbol": "€"
+            }
+        ],
+        "languages": [
+            {
+                "iso639_1": "ca",
+                "iso639_2": "cat",
+                "name": "Catalan",
+                "nativeName": "català"
+            }
+        ],
+        "translations": {
+            "br": "Andorra",
+            "pt": "Andorra",
+            "nl": "Andorra",
+            "hr": "Andora",
+            "fa": "آندورا",
+            "de": "Andorra",
+            "es": "Andorra",
+            "fr": "Andorre",
+            "ja": "アンドラ",
+            "it": "Andorra",
+            "hu": "Andorra"
+        },
+        "flag": "https://flagcdn.com/ad.svg",
+        "cioc": "AND",
+        "independent": true
+    },
+    {
+        "name": "Angola",
+        "topLevelDomain": [
+            ".ao"
+        ],
+        "alpha2Code": "AO",
+        "alpha3Code": "AGO",
+        "callingCodes": [
+            "244"
+        ],
+        "capital": "Luanda",
+        "altSpellings": [
+            "AO",
+            "República de Angola",
+            "ʁɛpublika de an'ɡɔla"
+        ],
+        "subregion": "Middle Africa",
+        "region": "Africa",
+        "population": 32866268,
+        "latlng": [
+            -12.5,
+            18.5
+        ],
+        "demonym": "Angolan",
+        "area": 1246700,
+        "gini": 51.3,
+        "timezones": [
+            "UTC+01:00"
+        ],
+        "borders": [
+            "COG",
+            "COD",
+            "ZMB",
+            "NAM"
+        ],
+        "nativeName": "Angola",
+        "numericCode": "024",
+        "flags": {
+            "svg": "https://flagcdn.com/ao.svg",
+            "png": "https://flagcdn.com/w320/ao.png"
+        },
+        "currencies": [
+            {
+                "code": "AOA",
+                "name": "Angolan kwanza",
+                "symbol": "Kz"
+            }
+        ],
+        "languages": [
+            {
+                "iso639_1": "pt",
+                "iso639_2": "por",
+                "name": "Portuguese",
+                "nativeName": "Português"
+            }
+        ],
+        "translations": {
+            "br": "Angola",
+            "pt": "Angola",
+            "nl": "Angola",
+            "hr": "Angola",
+            "fa": "آنگولا",
+            "de": "Angola",
+            "es": "Angola",
+            "fr": "Angola",
+            "ja": "アンゴラ",
+            "it": "Angola",
+            "hu": "Angola"
+        },
+        "flag": "https://flagcdn.com/ao.svg",
+        "regionalBlocs": [
+            {
+                "acronym": "AU",
+                "name": "African Union",
+                "otherNames": [
+                    "الاتحاد الأفريقي",
+                    "Union africaine",
+                    "União Africana",
+                    "Unión Africana",
+                    "Umoja wa Afrika"
+                ]
+            }
+        ],
+        "cioc": "ANG",
+        "independent": true
+    }, 
+];
+for(let i = 0; i < countries.length; i++)console.log(i, countries[i].name);
+const countryInfo = countries.map((country)=>{
+    return country.name;
+});
+console.log(countryInfo);
+const countryNames = countryInfo.filter((country)=>{
+    console.log(countryInfo.naam);
+});
+// Format in een functie plaatsen
+const ctryDetailsFormat = (currentItem)=>{
+    console.log('cur', currentItem);
+    return `
+        <ul class="ctry-details">
+            <li><h2>${currentItem.name}</h2></li>
+            <li>${currentItem.population}</li>
+            <li><img src="${currentItem.flag}" /></li>
+        </ul>
+        `;
+};
+const getAllCtryDetails = document.getElementById('all-ctry-details');
+console.log('dit is referentie', getAllCtryDetails);
+console.log('last', ctryDetailsFormat(countries[1]));
+getAllCtryDetails.innerHTML = `${ctryDetailsFormat(countries[1])}`;
+
+//# sourceMappingURL=index.de158e3a.js.map
